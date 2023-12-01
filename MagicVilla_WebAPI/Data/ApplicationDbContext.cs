790063215cc6ec474f4c95ba83cf47a59c18b77c
Eugen <zenyakolb@gmail.com>
2023-12-01 19:51:49 +0300

    Section 4 - Connection String and DbSet Entity

---
+++ MagicVilla_WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using MagicVilla_WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_WebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
