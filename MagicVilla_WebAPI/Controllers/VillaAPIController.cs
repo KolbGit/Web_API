5433c8eacabcd7e461de38937264640e14be7878
Eugen <zenyakolb@gmail.com>
2023-12-01 19:08:31 +0300

    Section 2 - Get individual Villa

--- MagicVilla_WebAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_WebAPI/Controllers/VillaAPIController.cs
@@ -14,5 +14,11 @@ namespace MagicVilla_WebAPI.Controllers
         {
             return VillaStore.villaList;
         }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
     }
 }
