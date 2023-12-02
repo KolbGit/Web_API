976abeb281988039d7815f3cd3f30ce74b0e5bc5
Eugen <zenyakolb@gmail.com>
2023-12-02 13:51:30 +0300

    Section 6 - Repository CleanUp

---
+++ MagicVilla_WebAPI/Repository/IRepostiory/IRepostiory.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace MagicVilla_WebAPI.Repository.IRepostiory
+{
+    public interface IRepostiory<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+        Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
+        Task CreateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task SaveAsync();
+    }
+}
