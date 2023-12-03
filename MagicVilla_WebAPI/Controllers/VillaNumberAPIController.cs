235964985e449597266404e1a273767d17a87cd9
Eugen <zenyakolb@gmail.com>
2023-12-03 14:27:05 +0300

    Section 7 - Special Validation and Base Service Update

--- MagicVilla_WebAPI/Controllers/VillaNumberAPIController.cs
+++ MagicVilla_WebAPI/Controllers/VillaNumberAPIController.cs
@@ -85,13 +85,13 @@ namespace MagicVilla_WebAPI.Controllers
             {
                 if (await _dbVillaNumber.GetAsync(u => u.VillaNo == createDTO.VillaNo) != null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa Number already Exists!");
+                    ModelState.AddModelError("ErrorMessages", "Villa Number already Exists!");
                     return BadRequest(ModelState);
                 }
 
                 if (await _dbVilla.GetAsync(u => u.Id == createDTO.VillaID) == null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa ID is Invalid!");
+                    ModelState.AddModelError("ErrorMessages", "Villa ID is Invalid!");
                     return BadRequest(ModelState);
                 }
 
@@ -160,7 +160,7 @@ namespace MagicVilla_WebAPI.Controllers
                 }
                 if (await _dbVilla.GetAsync(u => u.Id == updateDTO.VillaID) == null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa ID is Invalid!");
+                    ModelState.AddModelError("ErrorMessages", "Villa ID is Invalid!");
                     return BadRequest(ModelState);
                 }
 
