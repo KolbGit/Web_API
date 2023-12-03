6b4548353d16aacbe6f96120a20cf651d155ea01
Eugen <zenyakolb@gmail.com>
2023-12-03 20:16:31 +0300

    Section 9 - Auth Controller Action Methods

---
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDTO obj)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterationRequestDTO obj)
+        {
+            APIResponse result = await _authService.RegisterAsync<APIResponse>(obj);
+            if(result != null && result.IsSuccess)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
