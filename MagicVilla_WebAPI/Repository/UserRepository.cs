ff887278e3c119fde1e42bd6349f06e93c2ce81d
Eugen <zenyakolb@gmail.com>
2023-12-03 18:44:23 +0300

    Section 8 - User Controller

--- MagicVilla_WebAPI/Repository/UserRepository.cs
+++ MagicVilla_WebAPI/Repository/UserRepository.cs
@@ -37,7 +37,11 @@ namespace MagicVilla_WebAPI.Repository
 
             if (user == null)
             {
-                return null;
+                return new LoginResponseDTO()
+                {
+                    Token = "",
+                    User = null
+                };
             }
 
             //if user was found generate JWT Token
